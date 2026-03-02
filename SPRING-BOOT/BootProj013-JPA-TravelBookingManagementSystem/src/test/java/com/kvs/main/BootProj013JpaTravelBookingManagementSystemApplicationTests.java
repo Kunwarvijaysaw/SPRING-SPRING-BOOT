08e8fa5b35bbb35c6b502485c13599d0b1b72f39
Kunwar Vijay Saw <kunwarvijay54@gmail.com>
2026-03-02 12:46:41 +0530

    updated

---
+++ SPRING-BOOT/BootProj013-JPA-TravelBookingManagementSystem/src/test/java/com/kvs/main/BootProj013JpaTravelBookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvs.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj013JpaTravelBookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
