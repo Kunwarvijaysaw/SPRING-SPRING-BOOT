fd297941ed872637e87e0d13db91df18e89b2d8e
Kunwar Vijay Saw <kunwarvijay54@gmail.com>
2026-03-13 12:49:19 +0530

    ll

---
+++ SPRING-BOOT/BootProj022-FinderAndQuery-ProductReportApp/src/main/java/com/kvs/main/BootProj022FinderAndQueryProductReportAppApplication.java
@@ -0,0 +1,13 @@
+package com.kvs.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj022FinderAndQueryProductReportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj022FinderAndQueryProductReportAppApplication.class, args);
+	}
+
+}
