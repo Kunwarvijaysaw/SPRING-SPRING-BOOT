fdaee032773f51e3d9fbbd8d4011fd9e3929186e
Kunwar Vijay Saw <kunwarvijay54@gmail.com>
2026-03-16 11:32:58 +0530

    notes

---
+++ SPRING-BOOT/Test023-LOBs-LobStorageSystem/src/main/java/com/kvs/main/Test023LoBsLobStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kvs.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test023LoBsLobStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test023LoBsLobStorageSystemApplication.class, args);
+	}
+
+}
